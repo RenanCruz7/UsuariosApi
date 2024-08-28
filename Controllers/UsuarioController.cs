417834bf0304cf1a45ac2bc6dc2a493dd5233059
RenanCruz7 <renancruz199@gmail.com>
2024-08-28 19:49:24 -0300

    Migration e Criação de Service e injeção de dependencia

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,17 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using UsuariosApi.Data.Dtos;
+using UsuariosApi.Services;
 
 namespace UsuariosApi.Controllers
 {
-
     [ApiController]
     [Route("[Controller]")]
     public class UsuarioController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult CadastrarUsuario(CreateUsuarioDto dto)
+        private CadastroService _cadastroService;
+        public UsuarioController(CadastroService cadastroService)
         {
-            throw new NotImplementedException();
+            _cadastroService = cadastroService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CadastrarUsuario(CreateUsuarioDto dto)
+        {
+            await _cadastroService.Cadastra(dto);
+            return Ok("Usuario Cadastrado");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,19 +2,37 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using UsuariosApi.Data;
 using UsuariosApi.Models;
+using UsuariosApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 // Conection string para o banco de dados
 var connectionString = builder.Configuration.GetConnectionString("UsuarioConnection");
 
 // Conexao com o banco de dados
-builder.Services.AddDbContext<UsuarioDbContext>( opts =>
-    opts.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString)));
+builder.Services.AddDbContext<UsuarioDbContext>
+    (opts =>{opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));});
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<CadastroService>();
 
 builder.Services
     .AddIdentity<Usuario, IdentityRole>() // Adicionando conceito de identidade
     .AddEntityFrameworkStores<UsuarioDbContext>() //conecta com o banco de dados
     .AddDefaultTokenProviders(); // questao de autenticacao
+/*
+ * Configuração de senha por padrao são essas: 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Default Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
+*/
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
